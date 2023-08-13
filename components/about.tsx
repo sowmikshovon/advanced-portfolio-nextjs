512673508317d3d1b61fe6fded30f996d90eebce
Sowmik Shovon <sowmikshovon@gmail.com>
2023-08-13 19:29:06 +0600

    1st commit

---
+++ components/about.tsx
@@ -0,0 +1,48 @@
+"use client";
+
+import React from "react";
+import SectionHeading from "./section-heading";
+import { motion } from "framer-motion";
+import { useSectionInView } from "@/lib/hooks";
+
+export default function About() {
+  const { ref } = useSectionInView("About");
+
+  return (
+    <motion.section
+      ref={ref}
+      className="mb-28 max-w-[45rem] text-center leading-8 sm:mb-40 scroll-mt-28"
+      initial={{ opacity: 0, y: 100 }}
+      animate={{ opacity: 1, y: 0 }}
+      transition={{ delay: 0.175 }}
+      id="about"
+    >
+      <SectionHeading>About me</SectionHeading>
+      <p className="mb-3">
+        I have had passion for <span className="font-medium">Programming</span>{" "}
+        for a very long time, that's why I pursued BSc in{" "}
+        <span className="font-medium">Computer Science (CS)</span> from{" "}
+        <span className="font-medium">BRAC University</span>. After completing
+        graduation, I started working on improving my{" "}
+        <span className="font-medium">skills</span> by developing some{" "}
+        <span className="font-medium">personal projects</span>.{" "}
+        <span className="italic">My favorite part of programming</span> is the
+        problem-solving aspect. I <span className="underline">love</span> the
+        feeling of finally figuring out a solution to a problem. My core focus
+        is <span className="font-medium">Software development</span>. I am also
+        familiar with Web development, DevOps, and Software Quality Assurance. I
+        am always looking to learn new technologies. I am currently looking for
+        a <span className="font-medium">full-time position</span> as a software
+        developer.
+      </p>
+      <p>
+        <span className="italic">When I'm not coding</span>, I enjoy traveling,
+        watching youtube, movies, series, and photography. I also enjoy{" "}
+        <span className="font-medium">researching new things</span> that I
+        happened to come upon while watching something. I am currently learning
+        about{" "}
+        <span className="font-medium">space exploration🚀 and watches⌚</span>.
+      </p>
+    </motion.section>
+  );
+}
---
+++ lib/data.ts
@@ -0,0 +1,185 @@
+import React from "react";
+import { CgWorkAlt } from "react-icons/cg";
+import { FaReact } from "react-icons/fa";
+import { LuGraduationCap } from "react-icons/lu";
+import web1 from "@/public/web1.png";
+import web2 from "@/public/web2.png";
+import web3 from "@/public/web3.png";
+import web4 from "@/public/web4.png";
+import web5 from "@/public/web5.png";
+import web6 from "@/public/web6.png";
+import web7 from "@/public/web7.png";
+
+export const links = [
+  {
+    name: "Home",
+    hash: "#home",
+  },
+  {
+    name: "About",
+    hash: "#about",
+  },
+  {
+    name: "Projects",
+    hash: "#projects",
+  },
+  {
+    name: "Skills",
+    hash: "#skills",
+  },
+  {
+    name: "Education",
+    hash: "#education",
+  },
+  {
+    name: "Contact",
+    hash: "#contact",
+  },
+] as const;
+
+export const educationData = [
+  {
+    title: "SSC",
+    location: "Amtali A.K. Pilot Model High School",
+    description:
+      "I Passed Secondary School Certificate (SSC) from Amtali A.K. Pilot Model High School in 2015.",
+    icon: React.createElement(LuGraduationCap),
+    date: " 2015 ",
+  },
+  {
+    title: "HSC",
+    location: "Amtali Govt. College",
+    description:
+      "I Passed Higher Secondary Certificate (HSC) from Amtali Govt. College in 2017.",
+    icon: React.createElement(LuGraduationCap),
+    date: " 2017 ",
+  },
+  {
+    title: "BSc",
+    location: "BRAC University",
+    description:
+      "I completed my Bachelor in Science in Computer Science from BRAC University in 2022.",
+    icon: React.createElement(LuGraduationCap),
+    date: " 2017-2022 ",
+  },
+  {
+    title: "Skill Improvement",
+    location: "Dhaka, Bangladesh",
+    description:
+      "After graduation I took some time to learn more skills by developing some personal projects.",
+    icon: React.createElement(FaReact),
+    date: " 2022-2023 ",
+  },
+  {
+    title: "Looking for a Job",
+    location: "Dhaka, Bangladesh",
+    description: "Right now I am actively looking for a job.",
+    icon: React.createElement(CgWorkAlt),
+    date: " 2023-present ",
+  },
+] as const;
+
+export const projectsData = [
+  {
+    title: "Museum Of Candy",
+    description:
+      "I worked as a full-stack developer on this startup project for 2 years. Users can give public feedback to companies. (Click image for demo)",
+    tags: ["HTML", "CSS", "gh-pages"],
+    imageUrl: web1,
+    url: "https://sowmikshovon.github.io/Museum-of-Candy/",
+  },
+  {
+    title: "Gpt-3",
+    description:
+      "Job board for remote developer jobs. I was the front-end developer. It has features like filtering, sorting and pagination. (Click image for demo)",
+    tags: ["React", "CSS", "HTML", "Figma", "gh-pages"],
+    imageUrl: web2,
+    url: "https://sowmikshovon.github.io/gpt3_react/",
+  },
+  {
+    title: "Gericht",
+    description:
+      "A public web app for quick analytics on text. It shows word count, character count and social media post limits. (Click image for demo)",
+    tags: ["React", "CSS", "HTML", "Figma", "gh-pages"],
+    imageUrl: web3,
+    url: "https://sowmikshovon.github.io/restrurant_react/",
+  },
+  {
+    title: "Spotify-clone",
+    description:
+      "A public web app for quick analytics on text. It shows word count, character count and social media post limits. (Click image for demo)",
+    tags: [
+      "React",
+      "Next.js",
+      "TypeScript",
+      "Redux",
+      "PostgreSQL",
+      "Tailwind",
+      "Stripe",
+      "SupaBase",
+      "Vercel",
+    ],
+    imageUrl: web4,
+    url: "https://spotify-clone-2-omega.vercel.app/",
+  },
+  {
+    title: "Terminal Portfolio",
+    description:
+      "A public web app for quick analytics on text. It shows word count, character count and social media post limits. (Click image for demo)",
+    tags: ["JavaScript", "HTML", "CSS", "gh-page"],
+    imageUrl: web5,
+    url: "https://sowmikshovon.github.io/portfolio_terminal/",
+  },
+  {
+    title: "Portfolio-V1",
+    description:
+      "A public web app for quick analytics on text. It shows word count, character count and social media post limits. (Click image for demo)",
+    tags: ["React", "Next.js", "Tailwind", "Vercel"],
+    imageUrl: web6,
+    url: "https://sowmik-portfolio-v1.vercel.app/",
+  },
+  {
+    title: "Advanced Portfolio",
+    description:
+      "A public web app for quick analytics on text. It shows word count, character count and social media post limits. (Click image for demo)",
+    tags: [
+      "React",
+      "Next.js",
+      "TypeScript",
+      "Tailwind",
+      "Framer-motion",
+      "React Email",
+      "Resend",
+      "Vercel",
+    ],
+    imageUrl: web7,
+    url: "",
+  },
+] as const;
+
+export const skillsData = [
+  "Java",
+  "Python",
+  "HTML",
+  "CSS",
+  "JavaScript",
+  "React",
+  "TypeScript",
+  "Next.js",
+  "Vite",
+  "Node.js",
+  "Git",
+  "Tailwind",
+  "SCSS",
+  "MongoDB",
+  "Express",
+  "Redux",
+  "MySQL",
+  "PostgreSQL",
+  "Framer Motion",
+  "Android Development",
+  "Bash",
+  "Docker",
+  "Kubernetes",
+  "ASW",
+] as const;
